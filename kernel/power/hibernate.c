51a8f41611ba036997fe0dc6bdc83f1c675ce20e
Thomas Gleixner <tglx@linutronix.de>
2010-07-15 10:29:00 +0200

    suspend: Prevent might sleep splats
    timekeeping suspend/resume calls read_persistant_clock() which takes
    rtc_lock. That results in might sleep warnings because at that point
    we run with interrupts disabled.
    
    We cannot convert rtc_lock to a raw spinlock as that would trigger
    other might sleep warnings.
    
    As a temporary workaround we disable the might sleep warnings by
    setting system_state to SYSTEM_SUSPEND before calling sysdev_suspend()
    and restoring it to SYSTEM_RUNNING afer sysdev_resume().
    
    Needs to be revisited.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/power/hibernate.c
+++ kernel/power/hibernate.c
@@ -293,6 +293,8 @@ static int create_image(int platform_mode)
 
 	local_irq_disable();
 
+	system_state = SYSTEM_SUSPEND;
+
 	error = syscore_suspend();
 	if (error) {
 		pr_err("Some system devices failed to power down, aborting hibernation\n");
@@ -323,6 +325,7 @@ static int create_image(int platform_mode)
 	syscore_resume();
 
  Enable_irqs:
+	system_state = SYSTEM_RUNNING;
 	local_irq_enable();
 
  Enable_cpus:
@@ -456,6 +459,7 @@ static int resume_target_kernel(bool platform_mode)
 		goto Enable_cpus;
 
 	local_irq_disable();
+	system_state = SYSTEM_SUSPEND;
 
 	error = syscore_suspend();
 	if (error)
@@ -489,6 +493,7 @@ static int resume_target_kernel(bool platform_mode)
 	syscore_resume();
 
  Enable_irqs:
+	system_state = SYSTEM_RUNNING;
 	local_irq_enable();
 
  Enable_cpus:
@@ -574,6 +579,7 @@ int hibernation_platform_enter(void)
 		goto Enable_cpus;
 
 	local_irq_disable();
+	system_state = SYSTEM_SUSPEND;
 	syscore_suspend();
 	if (pm_wakeup_pending()) {
 		error = -EAGAIN;
@@ -586,6 +592,7 @@ int hibernation_platform_enter(void)
 
  Power_up:
 	syscore_resume();
+	system_state = SYSTEM_RUNNING;
 	local_irq_enable();
 
  Enable_cpus:
--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -466,6 +466,8 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 	arch_suspend_disable_irqs();
 	BUG_ON(!irqs_disabled());
 
+	system_state = SYSTEM_SUSPEND;
+
 	error = syscore_suspend();
 	if (!error) {
 		*wakeup = pm_wakeup_pending();
@@ -482,6 +484,8 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 		start_logging_wakeup_reasons();
 		syscore_resume();
 	}
+
+	system_state = SYSTEM_RUNNING;
 
 	arch_suspend_enable_irqs();
 	BUG_ON(irqs_disabled());
