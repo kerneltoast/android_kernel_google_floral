9eeb7fe60321121db3183e0ce0001619b9b1a59b
Peter Zijlstra <peterz@infradead.org>
2011-07-28 10:43:51 +0200

    mm, rt: kmap_atomic scheduling
    In fact, with migrate_disable() existing one could play games with
    kmap_atomic. You could save/restore the kmap_atomic slots on context
    switch (if there are any in use of course), this should be esp easy now
    that we have a kmap_atomic stack.
    
    Something like the below.. it wants replacing all the preempt_disable()
    stuff with pagefault_disable() && migrate_disable() of course, but then
    you can flip kmaps around like below.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    [dvhart@linux.intel.com: build fix]
    Link: http://lkml.kernel.org/r/1311842631.5890.208.camel@twins
    
    [tglx@linutronix.de: Get rid of the per cpu variable and store the idx
    		     and the pte content right away in the task struct.
    		     Shortens the context switch code. ]

--- include/linux/uaccess.h
+++ include/linux/uaccess.h
@@ -189,6 +189,7 @@ static __always_inline void pagefault_disabled_dec(void)
  */
 static inline void pagefault_disable(void)
 {
+	migrate_disable();
 	pagefault_disabled_inc();
 	/*
 	 * make sure to have issued the store before a pagefault
@@ -205,6 +206,7 @@ static inline void pagefault_enable(void)
 	 */
 	barrier();
 	pagefault_disabled_dec();
+	migrate_enable();
 }
 
 /*
