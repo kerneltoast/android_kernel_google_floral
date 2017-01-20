ec077c8e5984c49489e96f90a05b01cde6620328
Mike Galbraith <efault@gmx.de>
2017-01-20 18:10:20 +0100

    softirq: wake the timer softirq if needed
    The irq-exit path only checks the "normal"-softirq thread if it is
    running and ignores the state of the "timer"-softirq thread. It is possible
    that the timer-softirq thread is and has work pending which leads to the
    following warning:
    
    [   84.087571] NOHZ: local_softirq_pending 02
    [   84.087593] NOHZ: local_softirq_pending 02
    [   84.087598] NOHZ: local_softirq_pending 02
    [   84.087904] NOHZ: local_softirq_pending 02
    [   84.088526] NOHZ: local_softirq_pending 02
    [   84.088899] NOHZ: local_softirq_pending 02
    [   84.089463] NOHZ: local_softirq_pending 02
    [  115.013470] NOHZ: local_softirq_pending 02
    [  115.013601] NOHZ: local_softirq_pending 02
    [  115.013709] NOHZ: local_softirq_pending 02
    
    This was introduced during the timer-softirq split.
    
    Cc: stable-rt@vger.kernel.org
    Signed-off-by: Mike Galbraith <efault@gmx.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -28,6 +28,7 @@
 #include <linux/tick.h>
 #include <linux/locallock.h>
 #include <linux/irq.h>
+#include <linux/sched/types.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/irq.h>
@@ -784,10 +785,6 @@ void irq_enter(void)
 
 static inline void invoke_softirq(void)
 {
-#ifdef CONFIG_PREEMPT_RT_FULL
-	unsigned long flags;
-#endif
-
 #ifndef CONFIG_PREEMPT_RT_FULL
 	if (!force_irqthreads) {
 #ifdef CONFIG_HAVE_IRQ_EXIT_ON_IRQ_STACK
@@ -809,6 +806,7 @@ static inline void invoke_softirq(void)
 		wakeup_softirqd();
 	}
 #else /* PREEMPT_RT_FULL */
+	unsigned long flags;
 
 	local_irq_save(flags);
 	if (__this_cpu_read(ksoftirqd) &&
