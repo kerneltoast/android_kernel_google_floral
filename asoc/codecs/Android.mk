8c0f95481355ed706d6d1069fe22ed168c08a646
Robert Lee <lerobert@google.com>
2018-06-22 13:53:39 +0800

    asoc: cs35l36: add support for cs35l36 speaker amplifier
    Sync from b1/c1 project which base on below b1/c1 commit.
    - c9937ae audio: Add cirrus's settings recovery function for subsystem restart
    
    Change-Id: Ic5b1d01bd676a30350818d03771c112100b41748
    Signed-off-by: Robert Lee <lerobert@google.com>

--- asoc/codecs/Android.mk
+++ asoc/codecs/Android.mk
@@ -124,6 +124,14 @@ LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 ###########################################################
+include $(CLEAR_VARS)
+LOCAL_MODULE              := cs35l36.ko
+LOCAL_MODULE_KBUILD_NAME  := cs35l36_dlkm.ko
+LOCAL_MODULE_TAGS         := optional
+LOCAL_MODULE_DEBUG_ENABLE := true
+LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+###########################################################
 
 endif # DLKM check
 endif # supported target check
