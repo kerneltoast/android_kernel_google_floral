d0c7ee5948858b38c6f2f0bd952e70aff125c823
Arunk Khandavalli <akhandav@codeaurora.org>
2018-12-04 19:41:26 +0530

    qcacmn: Add support query per Vdev Beacon reception
    Add WMI support query per Vdev Beacon reception.
    
    Change-Id: I535c03eaefba9fbbcdc51cead1093680f451da10
    CRs-Fixed: 2360914
    Bug: 120964894
    Signed-off-by: Srinivas Girigowda <quic_sgirigow@quicinc.com>

--- wmi/inc/wmi_unified_param.h
+++ wmi/inc/wmi_unified_param.h
@@ -5566,6 +5566,7 @@ typedef enum {
 	wmi_apf_get_vdev_work_memory_resp_event_id,
 	wmi_roam_scan_stats_event_id,
 	wmi_wlan_sar2_result_event_id,
+	wmi_vdev_bcn_reception_stats_event_id,
 	wmi_events_max,
 } wmi_conv_event_id;
 
@@ -5999,6 +6000,7 @@ typedef enum {
 	wmi_service_per_vdev_chain_support,
 	wmi_service_new_htt_msg_format,
 	wmi_service_peer_unmap_cnf_support,
+	wmi_service_beacon_reception_stats,
 	wmi_services_max,
 } wmi_conv_service_ids;
 #define WMI_SERVICE_UNAVAILABLE 0xFFFF
