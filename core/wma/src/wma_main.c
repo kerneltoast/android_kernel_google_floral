db15ea36e35300bfb8080cd500c540bffcf4a940
Sourav Mohapatra <mohapatr@codeaurora.org>
2018-12-06 15:19:41 +0530

    qcacld-3.0: Feature flag for Wlan Latency Manager support
    WLAN Latency Manager support is one of the capabilities that is sent
    along with the service bits by firmware. Currently, driver is not
    exposing this feature to the user space.
    
    Create and add support for a feature flag to expose this feature to the
    userspace.
    
    Change-Id: Ica8213bdaeb32c7be01b6a71a7d8b7a32dc8611b
    CRs-Fixed: 2363704
    Bug: 119622871
    Signed-off-by: Srinivas Girigowda <quic_sgirigow@quicinc.com>

--- core/wma/src/wma_main.c
+++ core/wma/src/wma_main.c
@@ -4925,6 +4925,8 @@ static inline void wma_update_target_services(struct wmi_unified *wmi_handle,
 		cfg->twt_requestor = true;
 	if (wmi_service_enabled(wmi_handle, wmi_service_twt_responder))
 		cfg->twt_responder = true;
+	if (wmi_service_enabled(wmi_handle, wmi_service_vdev_latency_config))
+		g_fw_wlan_feat_caps |= (1 << VDEV_LATENCY_CONFIG);
 }
 
 /**
