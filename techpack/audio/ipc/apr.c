19afb7a16df70ee685550c1cf277030b5c59204c
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-25 20:57:37 -0800

    ASoC: msm: Fix compile errors when debugfs is disabled
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- techpack/audio/ipc/apr.c
+++ techpack/audio/ipc/apr.c
@@ -1124,9 +1124,9 @@ static int __init apr_debug_init(void)
 }
 #else
 static int __init apr_debug_init(void)
-(
+{
 	return 0;
-)
+}
 #endif
 
 static void apr_cleanup(void)
@@ -1147,7 +1147,9 @@ static void apr_cleanup(void)
 				mutex_destroy(&client[i][j].svc[k].m_lock);
 		}
 	}
+#ifdef CONFIG_DEBUG_FS
 	debugfs_remove(debugfs_apr_debug);
+#endif
 }
 
 static int apr_probe(struct platform_device *pdev)
