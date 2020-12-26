243286779d0b08868c047e9a04344008ba5ec19c
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-26 12:40:57 -0800

    qos: Replace cpumasks with atomic bitwise operations
    This is expensive and unneeded, especially since the qos functions are
    hot code paths. Mose of the cpumask functions use the bitmap API, which
    is also more expensive than just doing some simple operations on a word.
    
    Since we're operating with a CPU count that can fit within a word,
    replace the expensive cpumask operations with plain bitwise operations
    wherever possible to make the pm_qos framework more efficient.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4949,7 +4949,6 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 {
 	int status = -EINVAL;
 	struct resource *res;
-	int cpu;
 
 	status = _register_device(device);
 	if (status)
@@ -5077,13 +5076,7 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 		struct pm_qos_request *qos = &device->pwrctrl.l2pc_cpus_qos;
 
 		qos->type = PM_QOS_REQ_AFFINE_CORES;
-
-		cpumask_empty(&qos->cpus_affine);
-		for_each_possible_cpu(cpu) {
-			if ((1 << cpu) & device->pwrctrl.l2pc_cpus_mask)
-				cpumask_set_cpu(cpu, &qos->cpus_affine);
-		}
-
+		atomic_set(&qos->cpus_affine, device->pwrctrl.l2pc_cpus_mask);
 		pm_qos_add_request(&device->pwrctrl.l2pc_cpus_qos,
 				PM_QOS_CPU_DMA_LATENCY,
 				PM_QOS_DEFAULT_VALUE);
