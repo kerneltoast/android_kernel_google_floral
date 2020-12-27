3499e9ca8ff7aebd079c1238ffaa1067eefb5cfb
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-26 19:53:09 -0800

    Revert "kbuild: thin archives final link close --whole-archives option"
    This reverts commit 1328a1ae0e9048ff4b7f6b60c497db7a2799e1b1.
    
    This breaks GCC LTO.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -105,7 +105,6 @@ modpost_link()
 	if [ -n "${CONFIG_THIN_ARCHIVES}" ]; then
 		objects="--whole-archive				\
 			built-in.o					\
-			--no-whole-archive				\
 			--start-group					\
 			${KBUILD_VMLINUX_LIBS}				\
 			--end-group"
@@ -161,7 +160,6 @@ vmlinux_link()
 		if [[ -n "${CONFIG_THIN_ARCHIVES}" && -z "${CONFIG_LTO_CLANG}" ]]; then
 			objects="--whole-archive 			\
 				built-in.o				\
-				--no-whole-archive			\
 				--start-group				\
 				${KBUILD_VMLINUX_LIBS}			\
 				--end-group				\
@@ -180,7 +178,6 @@ vmlinux_link()
 		if [ -n "${CONFIG_THIN_ARCHIVES}" ]; then
 			objects="-Wl,--whole-archive			\
 				built-in.o				\
-				-Wl,--no-whole-archive			\
 				-Wl,--start-group			\
 				${KBUILD_VMLINUX_LIBS}			\
 				-Wl,--end-group				\
