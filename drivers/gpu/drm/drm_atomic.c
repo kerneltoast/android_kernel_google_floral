ffa897cadfa4dbf9bee77f516fbaf6348650c018
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-31 20:29:30 -0800

    drm: Boost DDR bus upon running an atomic ioctl
    In order to reduce jank, request a DDR bus boost whenever an atomic
    ioctl is sent, which is what our diplay HAL uses to commit frames to the
    display. The boost should be sufficient enough to render the display's
    maximum FPS without any dropped frames when there is no significant
    external source of load.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -30,6 +30,7 @@
 #include <drm/drm_atomic.h>
 #include <drm/drm_mode.h>
 #include <drm/drm_print.h>
+#include <linux/devfreq_boost.h>
 #include <linux/pm_qos.h>
 #include <linux/sync_file.h>
 
@@ -2386,6 +2387,7 @@ int drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 	int ret;
 
 	preempt_disable();
+	devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
 
 	/* Don't let the current task migrate to another CPU */
 	cpu = raw_smp_processor_id();
