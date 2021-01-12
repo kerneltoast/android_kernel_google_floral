f69681263857a11ec760b411f491b31819849806
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-12 00:02:35 -0800

    misc: ipu: Fix format specifier warnings
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/misc/ipu/ipu-adapter-ab-mfd.c
+++ drivers/misc/ipu/ipu-adapter-ab-mfd.c
@@ -408,7 +408,7 @@ static ssize_t pio_threshold_show(struct device *dev,
 {
 	struct ipu_adapter_ab_mfd_data *dev_data = dev_get_drvdata(dev);
 
-	return scnprintf(buf, PAGE_SIZE, "%u\n", dev_data->pio_threshold);
+	return scnprintf(buf, PAGE_SIZE, "%lu\n", dev_data->pio_threshold);
 }
 
 static ssize_t pio_threshold_store(struct device *dev,
