c9cf7be2f5be74820d7d6b00725c4a88a476fffc
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-11 23:59:05 -0800

    misc: airbrush: Fix unchecked must_check kfifo_*() return values
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/misc/airbrush/airbrush-sm-ctrl.c
+++ drivers/misc/airbrush/airbrush-sm-ctrl.c
@@ -2178,7 +2178,11 @@ static long ab_sm_async_notify(struct ab_sm_misc_session *sess,
 		reinit_completion(&sc->notify_comp);
 	}
 
-	kfifo_out(sc->async_entries, &chip_state, sizeof(chip_state));
+	ret = kfifo_out(sc->async_entries, &chip_state, sizeof(chip_state));
+	if (ret) {
+		mutex_unlock(&sc->async_fifo_lock);
+		return ret;
+	}
 
 	if (copy_to_user((void __user *)arg,
 				&chip_state, sizeof(chip_state))) {
@@ -2197,15 +2201,21 @@ static int ab_sm_misc_open(struct inode *ip, struct file *fp)
 	struct miscdevice *misc_dev = fp->private_data;
 	struct ab_state_context *sc =
 		container_of(misc_dev, struct ab_state_context, misc_dev);
+	int ret;
 
 	sess = kzalloc(sizeof(struct ab_sm_misc_session), GFP_KERNEL);
 	if (!sess)
 		return -ENOMEM;
 
+	ret = kfifo_alloc(&sess->async_entries,
+		AB_KFIFO_ENTRY_SIZE * sizeof(int), GFP_KERNEL);
+	if (ret) {
+		kfree(sess);
+		return ret;
+	}
+
 	sess->sc = sc;
 	sess->first_entry = true;
-	kfifo_alloc(&sess->async_entries,
-		AB_KFIFO_ENTRY_SIZE * sizeof(int), GFP_KERNEL);
 
 	fp->private_data = sess;
 
@@ -2915,6 +2925,11 @@ int ab_sm_init(struct platform_device *pdev)
 	if (ab_sm_ctx == NULL)
 		goto fail_mem_alloc;
 
+	ret = kfifo_alloc(&ab_sm_ctx->state_change_reqs,
+		AB_KFIFO_ENTRY_SIZE * sizeof(struct ab_change_req), GFP_KERNEL);
+	if (ret)
+		goto fail_kfifo_alloc;
+
 	ab_sm_ctx->pdev = pdev;
 	ab_sm_ctx->dev = &pdev->dev;
 	dev_set_drvdata(ab_sm_ctx->dev, ab_sm_ctx);
@@ -3035,8 +3050,6 @@ int ab_sm_init(struct platform_device *pdev)
 	init_completion(&ab_sm_ctx->transition_comp);
 	init_completion(&ab_sm_ctx->notify_comp);
 	init_completion(&ab_sm_ctx->shutdown_comp);
-	kfifo_alloc(&ab_sm_ctx->state_change_reqs,
-		AB_KFIFO_ENTRY_SIZE * sizeof(struct ab_change_req), GFP_KERNEL);
 
 	ab_sm_ctx->cold_boot = true;
 	ab_sm_ctx->el2_mode = 0;
@@ -3103,6 +3116,8 @@ fail_ab_ready:
 fail_pmic_resources:
 	misc_deregister(&ab_sm_ctx->misc_dev);
 fail_misc_reg:
+	kfifo_free(&ab_sm_ctx->state_change_reqs);
+fail_kfifo_alloc:
 	devm_kfree(dev, (void *)ab_sm_ctx);
 	ab_sm_ctx = NULL;
 fail_mem_alloc:
