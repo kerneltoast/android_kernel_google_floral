dc391fa8081b5968f3384dd9fab18371782a95a0
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-12 00:10:34 -0800

    google_battery: Fix unused variable warnings when debugfs is disabled
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/power/supply/google/google_charger.c
+++ drivers/power/supply/google/google_charger.c
@@ -3016,7 +3016,9 @@ DEFINE_SIMPLE_ATTRIBUTE(bd_enabled_fops, bd_enabled_get,
 static int chg_init_fs(struct chg_drv *chg_drv)
 {
 	int ret;
+#ifdef CONFIG_DEBUG_FS
 	struct dentry *de = NULL;
+#endif
 
 	ret = device_create_file(chg_drv->device, &dev_attr_charge_stop_level);
 	if (ret != 0) {
