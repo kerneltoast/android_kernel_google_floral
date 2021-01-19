2d903dfbedfc4a53475ba914381531c2827cd072
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-19 09:51:43 -0800

    arm64: Move LSE atomics out from the alternatives framework
    We know at compile time whether or not our CPU supports LSE atomics,
    so the alternatives framework is only a hindrance, and can reduce
    performance with unnecessary LL/SC register clobbers. This cleans up the
    LSE atomics to depend solely on CONFIG_ARM64_LSE_ATOMICS for determining
    if these instructions are supported. The ARM64_LSE_ATOMIC_INSN macro is
    now gone, and any remaining prefetch instructions that were intended for
    LL/SC via ARM64_LSE_ATOMIC_INSN are now rolled into the LSE usecase,
    since LSE atomics still benefit from prefetching.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- arch/arm64/include/asm/spinlock.h
+++ arch/arm64/include/asm/spinlock.h
@@ -37,16 +37,10 @@ static inline void arch_spin_lock(arch_spinlock_t *lock)
 	asm volatile(
 	/* Atomically increment the next ticket. */
 	ARM64_LSE_ATOMIC_INSN(
-	/* LL/SC */
-"	prfm	pstl1strm, %3\n"
-"1:	ldaxr	%w0, %3\n"
-"	add	%w1, %w0, %w5\n"
-"	stxr	%w2, %w1, %3\n"
-"	cbnz	%w2, 1b\n",
 	/* LSE atomics */
+"	prfm	pstl1strm, %3\n"
 "	mov	%w2, %w5\n"
 "	ldadda	%w2, %w0, %3\n"
-	__nops(3)
 	)
 
 	/* Did we get the lock? */
@@ -74,16 +68,8 @@ static inline int arch_spin_trylock(arch_spinlock_t *lock)
 	arch_spinlock_t lockval;
 
 	asm volatile(ARM64_LSE_ATOMIC_INSN(
-	/* LL/SC */
-	"	prfm	pstl1strm, %2\n"
-	"1:	ldaxr	%w0, %2\n"
-	"	eor	%w1, %w0, %w0, ror #16\n"
-	"	cbnz	%w1, 2f\n"
-	"	add	%w0, %w0, %3\n"
-	"	stxr	%w1, %w0, %2\n"
-	"	cbnz	%w1, 1b\n"
-	"2:",
 	/* LSE atomics */
+	"	prfm	pstl1strm, %2\n"
 	"	ldr	%w0, %2\n"
 	"	eor	%w1, %w0, %w0, ror #16\n"
 	"	cbnz	%w1, 1f\n"
@@ -104,14 +90,10 @@ static inline void arch_spin_unlock(arch_spinlock_t *lock)
 	unsigned long tmp;
 
 	asm volatile(ARM64_LSE_ATOMIC_INSN(
-	/* LL/SC */
-	"	ldrh	%w1, %0\n"
-	"	add	%w1, %w1, #1\n"
-	"	stlrh	%w1, %0",
 	/* LSE atomics */
 	"	mov	%w1, #1\n"
 	"	staddlh	%w1, %0\n"
-	__nops(1))
+	)
 	: "=Q" (lock->owner), "=&r" (tmp)
 	:
 	: "memory");
