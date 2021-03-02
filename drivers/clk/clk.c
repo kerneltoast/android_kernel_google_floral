67ade0e1a2161e1a2e3d1c5809a7f7babd3e6681
Sultan Alsawaf <sultan@kerneltoast.com>
2021-03-01 22:28:42 -0800

    clk: Fix compile errors when debugfs is disabled
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -51,11 +51,6 @@ static struct hlist_head *all_lists[] = {
 	NULL,
 };
 
-static struct hlist_head *orphan_list[] = {
-	&clk_orphan_list,
-	NULL,
-};
-
 struct clk_handoff_vdd {
 	struct list_head list;
 	struct clk_vdd_class *vdd_class;
@@ -2704,6 +2699,11 @@ static int inited = 0;
 static u32 debug_suspend;
 static DEFINE_MUTEX(clk_debug_lock);
 static HLIST_HEAD(clk_debug_list);
+
+static struct hlist_head *orphan_list[] = {
+	&clk_orphan_list,
+	NULL,
+};
 
 static void clk_state_subtree(struct clk_core *c)
 {
