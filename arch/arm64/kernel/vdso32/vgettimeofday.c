0af7f98c67447fafd26dd622f6a2a7928ba71da3
Sultan Alsawaf <sultan@kerneltoast.com>
2021-11-07 22:27:05 -0800

    arm64: vdso32: Fix compile errors due to unwanted atomic header inclusion
    The bitops headers now use generic atomics, which doesn't mix with vdso.
    The vdso code still needs some simple bitops macros, so just stave off
    the unwanted header inclusion by defining the headers' respective header
    guards.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- arch/arm64/kernel/vdso32/vgettimeofday.c
+++ arch/arm64/kernel/vdso32/vgettimeofday.c
@@ -1,3 +1,6 @@
+#define _ASM_GENERIC_BITOPS_ATOMIC_H_
+#define _ASM_GENERIC_BITOPS_LE_H_
+#define _ASM_GENERIC_BITOPS_LOCK_H_
 #include "compiler.h"
 #include "datapage.h"
 #include "../../../../lib/vdso/vgettimeofday.c"
