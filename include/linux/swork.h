b7d8dcdbe02dd09a06406cd21556932a648049eb
Sultan Alsawaf <sultan@kerneltoast.com>
2022-03-01 00:25:52 -0800

    work-simple: Rewrite for clarity and performance
    This implementation suffers from a race on init where a timer interrupt may
    occur before `glob_worker` is set, causing the kernel to panic as a result.
    Upon closer inspection, all of the kthread juggling inside the swork code
    appears to make no sense, and the get/put semantics are also meaningless
    considering that there are core kernel users that do a swork_get() on init
    and then never do a swork_put(). A spin lock is also used for a linked list
    that could simply be a lock-less list instead.
    
    Rewrite the swork code to remove the get/put semantics and speed things up
    with a lock-less list. Now this code truly is simple.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/swork.h
+++ include/linux/swork.h
@@ -1,10 +1,10 @@
 #ifndef _LINUX_SWORK_H
 #define _LINUX_SWORK_H
 
-#include <linux/list.h>
+#include <linux/llist.h>
 
 struct swork_event {
-	struct list_head item;
+	struct llist_node item;
 	unsigned long flags;
 	void (*func)(struct swork_event *);
 };
@@ -18,7 +18,7 @@ static inline void INIT_SWORK(struct swork_event *event,
 
 bool swork_queue(struct swork_event *sev);
 
-int swork_get(void);
-void swork_put(void);
+static inline int swork_get(void) { return 0; }
+static inline void swork_put(void) { }
 
 #endif /* _LINUX_SWORK_H */
