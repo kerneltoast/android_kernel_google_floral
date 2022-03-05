cbdfb323f9cae2890aef636c6528a1315e584328
Sultan Alsawaf <sultan@kerneltoast.com>
2022-03-04 23:44:29 -0800

    genirq: Fix spurious "handler enabled interrupts" warning on RT
    The SDE IRQ handler routes interrupts itself using generic_handle_irq(),
    which coughs up a warning on RT since the SDE IRQ is threaded and therefore
    IRQs aren't disabled in the handler. Aside from the warning spam, this is
    problematic because the warning unevenly disables IRQs thinking that IRQs
    are supposed to be disabled to begin with, which results in all sorts of
    mayhem. To fix it, check if IRQs were actually disabled prior to running a
    given handler instead of assuming it by default.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/irq/handle.c
+++ kernel/irq/handle.c
@@ -142,13 +142,15 @@ irqreturn_t __handle_irq_event_percpu(struct irq_desc *desc, unsigned int *flags
 	record_irq_time(desc);
 
 	for_each_action_of_desc(desc, action) {
+		bool irqs_were_off = irqs_disabled();
 		irqreturn_t res;
 
 		trace_irq_handler_entry(irq, action);
 		res = action->handler(irq, action->dev_id);
 		trace_irq_handler_exit(irq, action, res);
 
-		if (WARN_ONCE(!irqs_disabled(),"irq %u handler %pF enabled interrupts\n",
+		if (WARN_ONCE(irqs_were_off && !irqs_disabled(),
+			      "irq %u handler %pF enabled interrupts\n",
 			      irq, action->handler))
 			local_irq_disable();
 
