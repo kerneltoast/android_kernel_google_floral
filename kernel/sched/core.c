8d5514395f2b3cda699cf0616e301104c2f220a3
Sultan Alsawaf <sultan@kerneltoast.com>
2022-04-20 01:14:18 -0700

    sched: Set sched_nr_migrate back to 32 on RT for Android
    Android isn't a real-time userspace and has lots of processes, which makes
    the normal sched_nr_migrate value of 32 more appealing. In addition,
    there's no observed latency reduction from using a sched_nr_migrate value
    of 8, probably because the shallowest idle state on mobile CPUs takes
    longer to enter/exit than it takes for the scheduler to do a load balance
    run, so our tail end latency is limited by cpuidle anyway.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -55,7 +55,7 @@ DEFINE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
  * Number of tasks to iterate in a single balance run.
  * Limited because this is done with IRQs disabled.
  */
-#ifndef CONFIG_PREEMPT_RT_FULL
+#if !defined(CONFIG_PREEMPT_RT_FULL) || defined(CONFIG_ANDROID)
 const_debug unsigned int sysctl_sched_nr_migrate = 32;
 #else
 const_debug unsigned int sysctl_sched_nr_migrate = 8;
