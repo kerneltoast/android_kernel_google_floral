79858cae294aa36af3cbc53f4bc868377ed5f15c
Sultan Alsawaf <sultan@kerneltoast.com>
2022-04-24 15:38:45 -0700

    printk: Allow logs to be flushed from IRQ context during a panic on RT
    Once a panic happens, scheduling no longer matters and all we care about is
    getting debug information. As it stands, crucial panic messages are often
    lost on RT because printk refuses to flush the log buffers from IRQ/NMI
    context, which is a nightmare for debugging. Fix it by allowing flushes
    when oops_in_progress is set.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1796,7 +1796,7 @@ static void call_console_drivers(const char *ext_text, size_t ext_len,
 	if (!console_drivers)
 		return;
 
-	if (IS_ENABLED(CONFIG_PREEMPT_RT_BASE)) {
+	if (IS_ENABLED(CONFIG_PREEMPT_RT_BASE) && !oops_in_progress) {
 		if (in_irq() || in_nmi())
 			return;
 	}
@@ -2569,11 +2569,6 @@ void console_unblank(void)
 {
 	struct console *c;
 
-	if (IS_ENABLED(CONFIG_PREEMPT_RT_BASE)) {
-		if (in_irq() || in_nmi())
-			return;
-	}
-
 	/*
 	 * console_unblank can no longer be called in interrupt context unless
 	 * oops_in_progress is set to 1..
@@ -2581,8 +2576,13 @@ void console_unblank(void)
 	if (oops_in_progress) {
 		if (down_trylock_console_sem() != 0)
 			return;
-	} else
+	} else {
+		if (IS_ENABLED(CONFIG_PREEMPT_RT_BASE)) {
+			if (in_irq() || in_nmi())
+				return;
+		}
 		console_lock();
+	}
 
 	console_locked = 1;
 	console_may_schedule = 0;
