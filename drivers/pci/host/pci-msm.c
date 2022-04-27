96396da0e3dfbdbb3bd88898100d1ac115abd1f6
Sultan Alsawaf <sultan@kerneltoast.com>
2022-04-27 00:59:09 -0700

    msm: pcie: Fix missing mutex unlock on error in msm_pcie_enable()
    Although it looks like this error condition will never happen on a
    production device, make sure it unlocks the setup_lock mutex anyway.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/pci/host/pci-msm.c
+++ drivers/pci/host/pci-msm.c
@@ -4322,7 +4322,7 @@ static int msm_pcie_enable(struct msm_pcie_dev_t *dev, u32 options)
 			PCIE_ERR(dev,
 				"PCIe: RC%d: unsupported gen speed: %d\n",
 				dev->rc_idx, current_link_speed);
-			return 0;
+			goto out;
 		}
 
 		bw_scale = &dev->bw_scale[index];
