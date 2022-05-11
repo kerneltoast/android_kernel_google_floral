5ab58b8a87ca733df77fe735688a030665ddd7a7
Sultan Alsawaf <sultan@kerneltoast.com>
2022-05-11 01:40:17 -0700

    smp: Use migrate disable/enable in smp_call_function_single_async()
    Asynchronous IPI users must already handle csd object lifetimes on their
    own, so there's no need to prevent re-entrancy on a single CPU inside
    smp_call_function_single_async(). As such, smp_call_function_single_async()
    can be made more RT-friendly by just using migrate enable/disable instead
    of disabling preemption, since preventing migration is all that's needed.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/smp.c
+++ kernel/smp.c
@@ -359,7 +359,7 @@ int smp_call_function_single_async(int cpu, call_single_data_t *csd)
 {
 	int err = 0;
 
-	preempt_disable();
+	migrate_disable();
 
 	/* We could deadlock if we have to wait here with interrupts disabled! */
 	if (WARN_ON_ONCE(csd->flags & CSD_FLAG_LOCK))
@@ -369,7 +369,7 @@ int smp_call_function_single_async(int cpu, call_single_data_t *csd)
 	smp_wmb();
 
 	err = generic_exec_single(cpu, csd, csd->func, csd->info);
-	preempt_enable();
+	migrate_enable();
 
 	return err;
 }
