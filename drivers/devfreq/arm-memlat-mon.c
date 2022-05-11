91ee07604946ae1a284475020d5ecf3f04b699f9
Sultan Alsawaf <sultan@kerneltoast.com>
2022-05-10 21:38:01 -0700

    Revert "memlat: Optimize perf event reads when possible"
    This reverts commit 190be9ed11703e8e0f7c79461662634792c9beb4.
    
    There are a number of problems with this optimization attempt, such as how
    a perf error code could be returned to one of the counter values, a CPU
    could enter an SCM call for just one event and result in garbage
    computations or a divide-by-zero when calculating the stall percentage,
    and IRQs are disabled as a heavy-handed way of preventing CPU migration.
    
    Toss this commit out so that it can be replaced by something better.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/devfreq/arm-memlat-mon.c
+++ drivers/devfreq/arm-memlat-mon.c
@@ -49,7 +49,6 @@ enum ev_index {
 struct event_data {
 	struct perf_event *pevent;
 	unsigned long prev_count;
-	bool any_cpu_readable;
 };
 
 struct cpu_pmu_stats {
@@ -102,37 +101,7 @@ static inline unsigned long read_event(struct event_data *event)
 	if (!event->pevent)
 		return 0;
 
-	if (event->any_cpu_readable) {
-		if (perf_event_read_local(event->pevent, &total))
-			return 0;
-	} else {
-		unsigned int ev_cpu = READ_ONCE(event->pevent->oncpu);
-		bool local_read;
-		int ret;
-
-		if (ev_cpu >= nr_cpu_ids)
-			return 0;
-
-		local_irq_disable();
-		if ((local_read = (ev_cpu == raw_smp_processor_id())))
-			ret = perf_event_read_local(event->pevent, &total);
-		local_irq_enable();
-
-		if (!local_read) {
-			/*
-			 * Some SCM calls take very long (20+ ms), so the perf
-			 * event IPI could lag on the CPU running the SCM call.
-			 */
-			if (under_scm_call(ev_cpu))
-				return 0;
-
-			total = perf_event_read_value(event->pevent, &enabled,
-						      &running);
-		} else if (ret) {
-			return ret;
-		}
-	}
-
+	total = perf_event_read_value(event->pevent, &enabled, &running);
 	ev_count = total - event->prev_count;
 	event->prev_count = total;
 	return ev_count;
@@ -161,6 +130,14 @@ static unsigned long get_cnt(struct memlat_hwmon *hw)
 {
 	int cpu;
 	struct cpu_grp_info *cpu_grp = to_cpu_grp(hw);
+
+	/*
+	 * Some of SCM call is very heavy(+20ms) so perf IPI could
+	 * be stuck on the CPU which contributes long latency.
+	 */
+	if (under_scm_call()) {
+		return 0;
+	}
 
 	for_each_cpu(cpu, &cpu_grp->cpus)
 		read_perf_counters(cpu, cpu_grp);
@@ -217,7 +194,6 @@ static struct perf_event_attr *alloc_attr(void)
 
 static int set_events(struct cpu_grp_info *cpu_grp, int cpu)
 {
-	static struct cpumask all_cpu_mask = CPU_MASK_ALL;
 	struct perf_event *pevent;
 	struct perf_event_attr *attr;
 	int err, i;
@@ -241,8 +217,6 @@ static int set_events(struct cpu_grp_info *cpu_grp, int cpu)
 			goto err_out;
 		cpustats->events[i].pevent = pevent;
 		perf_event_enable(pevent);
-		cpustats->events[i].any_cpu_readable =
-			cpumask_equal(&pevent->readable_on_cpus, &all_cpu_mask);
 	}
 
 	kfree(attr);
